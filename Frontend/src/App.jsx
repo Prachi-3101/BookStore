8525a1d3effa1a5805faae978bb99360ca2184cc
Prachi-3101 <prachistudies22@gmail.com>
2026-03-23 22:41:19 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    <Footer />
+    </>
+  )
+}
+
+export default App
